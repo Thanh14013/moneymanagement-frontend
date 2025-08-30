802a6196331157ef789cb75002bae2fe28064bda
Nguyen Vu Thanh <thanh14704@gmail.com>
2025-08-30 22:11:10 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Category from './pages/Category'
+import Income from './pages/Income'
+import Expense from './pages/Expense'
+import Filter from './pages/Filter'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">Money Manager</div>
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/expense" element={<Expense />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
